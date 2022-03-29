b562f91c32fa9cfc539c5cb211cc47c29bf115a2
yurikuit <yuri.kuit+github@gmail.com>
2022-03-29 13:49:02 +0200

    update

--- 1_git-v4.sh.sh
+++ 1_git-v4.sh.sh
@@ -3,6 +3,7 @@
 ##################################################################################################################
 # Author 	: Erik Dubois
 # Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
 # Website	: https://www.arcolinux.info
 # Website	: https://www.arcolinux.com
 # Website	: https://www.arcolinuxd.com
@@ -14,6 +15,27 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# change a commit comment
+# git commit --amend -m "more info"
+# git push --force origin
+
+# stash or commit
+# git reset --hard
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
 
 # Below command will backup everything inside the project folder
 git add --all .
@@ -23,8 +45,7 @@ echo "####################################"
 echo "Write your commit comment!"
 echo "####################################"
 
-#read input
-input="update"
+read input
 
 # Committing to the local repository with a message containing the time details and commit text
 
@@ -32,7 +53,15 @@ git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin main
+if grep -q main .git/config; then
+	echo "Using main"
+	git push -u origin main
+fi
+
+if grep -q master .git/config; then
+	echo "Using master"
+	git push -u origin master
+fi
 
 echo "################################################################"
 echo "###################    Git Push Done      ######################"
